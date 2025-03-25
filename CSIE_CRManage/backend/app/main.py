59a6ffba6dab451b0ee82ad36d0babba0ed4d54c
J.D.I.哲哲 <88202252+DazzleDavid@users.noreply.github.com>
2025-03-26 00:57:47 +0800

    David 20250319 (#2)
    (feat): Initialize backend with database configuration and core application structure

---
+++ CSIE_CRManage/backend/app/main.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from fastapi import FastAPI, Depends
+from sqlmodel import Session, select
+
+# from .models import User
+from .database import create_db_and_tables, get_session
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+# @app.post("/users/", response_model=User)
+# def create_user(user: User, session: Session = Depends(get_session)):
+#     session.add(user)
+#     session.commit()
+#     session.refresh(user)
+#     return user
+
+# @app.get("/users/", response_model=list[User])
+# def read_users(session: Session = Depends(get_session)):
+#     users = session.exec(select(User)).all()
+#     return users
