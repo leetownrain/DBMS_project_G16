840ff67f31af67e3b15e67e99d9d924d1296f3ac
DazzleDavid <david700707@gmail.com>
2025-03-30 23:40:38 +0800

    (refactor): remove unused backend files and clean up main.py

--- CSIE_CRManage/backend/app/main.py
+++ CSIE_CRManage/backend/app/main.py
@@ -19,22 +19,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
-
-
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
-
-# @app.post("/users/", response_model=User)
-# def create_user(user: User, session: Session = Depends(get_session)):
-#     session.add(user)
-#     session.commit()
-#     session.refresh(user)
-#     return user
-
-# @app.get("/users/", response_model=list[User])
-# def read_users(session: Session = Depends(get_session)):
-#     users = session.exec(select(User)).all()
-#     return users
